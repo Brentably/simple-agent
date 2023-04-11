e896c7968533d4831d79ecaa747eaa70a4fee3bb
Brentably <Brent@pabnet.com>
2023-04-11 16:01:30 -0700

    improved chat for readability

--- src/chat.ts
+++ src/chat.ts
@@ -7,10 +7,10 @@ export default async function chat(model = "gpt-3.5-turbo") {
   console.log('\x1b[1m%s\x1b[0m', `Chat GPT CLI.`);
   console.log(` Model: ${chalk.green(model)} \n`)
     while(true) {
-      const value = await getUserInput(">>> ")
+      const value = await getUserInput(chalk.greenBright(">>> "))
     const resp = await getChatCompletion(value)
 
-    console.log(highlightCode(resp))
+    console.log("\n" + highlightCode(resp) + "\n")
     }
 }
 
