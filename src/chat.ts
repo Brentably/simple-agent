031cdad3d3894096c8876153a1219a7b3f5cdf73
Brentably <Brent@pabnet.com>
2023-04-11 13:54:24 -0700

    added working code highlighting

--- src/chat.ts
+++ src/chat.ts
@@ -1,7 +1,7 @@
 import readline from 'readline'
 import { getChatCompletion } from './openai';
-import chalk from 'chalk';
-
+import { highlightCode } from './utils/highlight';
+import chalk from 'chalk'
 
 export default async function chat(model = "gpt-3.5-turbo") {
   console.log('\x1b[1m%s\x1b[0m', `Chat GPT CLI.`);
@@ -10,34 +10,7 @@ export default async function chat(model = "gpt-3.5-turbo") {
       const value = await getUserInput(">>> ")
     const resp = await getChatCompletion(value)
 
-    // const codeSnippet = 'function greet(name) {\n  return `Hello, ${name}! orange`;\n}';
-
-    // Define the syntax highlighting styles for each language element
-    const styles = {
-      keyword: chalk.keyword('orange'),
-      string: chalk.green,
-      punctuator: chalk.yellow,
-      function: chalk.blue,
-    };
-
-    // Use a regular expression to match each language element and apply the corresponding style
-    const highlightedCode = resp.replace(/(\b\w+\b|[{}()[\],;]|`.*?`)/g, (match) => {
-      if (/^\d/.test(match)) {
-        return chalk.yellow(match); // highlight numbers with yellow
-      }
-      for (const [name, color] of Object.entries(styles)) {
-        if (new RegExp(`^${name}$`).test(match)) {
-          return color.inverse.bold(match); // highlight matching words with an inverse and bold color
-        }
-      }
-      return match;
-    });
-
-    // Log the highlighted code to the console
-    console.log(highlightedCode)
-
-
-    // console.log(resp)
+    console.log(highlightCode(resp))
     }
 }
 
@@ -57,3 +30,4 @@ export async function getUserInput(prefix: string):Promise<string> {
 
   return answer
 }
+
